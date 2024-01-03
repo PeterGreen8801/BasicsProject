4beed597824b320edd191a3439aac9970b44c8cc
Peter <petergreen880@yahoo.com>
2024-01-02 20:54:46 -0500

    Added Collision Detection to Balloons

--- BasicsProjectC#/Assets/Scripts/Dart.cs
+++ BasicsProjectC#/Assets/Scripts/Dart.cs
@@ -6,9 +6,24 @@ public class Dart : MonoBehaviour
 {
     public float speed = 10f;
 
+    void Start()
+    {
+        Destroy(gameObject, 5f);
+    }
+
     // Update is called once per frame
     void Update()
     {
         gameObject.transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Balloon"))
+        {
+            Debug.Log("Hit");
+            Destroy(collision.gameObject);
+
+        }
     }
 }
